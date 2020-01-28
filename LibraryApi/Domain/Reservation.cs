ae21838cd1e04929df250ac350f482b2150709b1
Sam Petitti <sampjam@gmail.com>
2020-01-28 20:15:30 +0000

    end of day 2

--- LibraryApi/Domain/Reservation.cs
+++ LibraryApi/Domain/Reservation.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace LibraryApi.Domain
 {
     public class Reservation
     {
+        [JsonConverter(typeof(JsonStringEnumConverter))]
         public enum ReservationStatus {  Pending, Approved, Cancelled }
         public int Id { get; set; }
         public string For { get; set; }
